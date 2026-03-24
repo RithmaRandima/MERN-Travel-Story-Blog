a2346545691e2628dc097ea17c96f1f688c3f455
RithmaRandima <rithma.r.nandasiri@gmail.com>
2026-03-24 14:05:45 +0530

    Complete createAccount controller

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,77 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+export const createAccount = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName.trim() || !email.trim() || !password.trim()) {
+      return res
+        .status(400)
+        .json({ error: true, message: "All Fields are required!" });
+    }
+
+    const existsUser = await userModel.findOne({ email });
+    if (existsUser) {
+      return res
+        .status(400)
+        .json({ error: true, message: "User already exists!" });
+    }
+    // email check
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({ error: true, message: "Please enter valid Email!" });
+    }
+
+    // password check
+    if (
+      !validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+    ) {
+      return res.status(400).json({
+        error: true,
+        message:
+          "Password must be at least 8 characters and include uppercase, lowercase, number and symbol",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userModel({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "5h",
+      },
+    );
+
+    res.status(201).json({
+      error: false,
+      user: { fullName: user.fullName, email: user.email },
+      accessToken,
+      message: "Registration Successfull!",
+    });
+  } catch (error) {
+    console.log("Error in createAccount function!", error);
+    return res.status(400).json({
+      error: true,
+      message: "Error on Server",
+    });
+    process.exit(1);
+  }
+};
